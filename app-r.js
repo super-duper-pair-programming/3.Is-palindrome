c19d8ee8580f250a855a235da60ff0d33cb38d76
chaerin-dev <chaerin.dev@gmail.com>
2022-09-23 12:35:06 +0900

    refactor: refactoring

--- app-r.js
+++ app-r.js
@@ -8,19 +8,26 @@ const isPalindrome = str => {
   return processedStr === reversedProcessedStr;
 };
 
+const $palindromeForm = document.querySelector('.palindrome-checker');
 const $palindromeInput = document.querySelector('.palindrome-input');
+const $palindromeResult = document.querySelector('.palindrome-result');
 
-const render = input => {
+const renderResult = input => {
   if (input.trim() !== '') {
-    document.querySelector('.palindrome-result').textContent = `
+    $palindromeResult.textContent = `
     "${input}" is ${isPalindrome(input) ? '' : 'not'} a palindrome`;
   }
 
-  $palindromeInput.value = '';
+  $palindromeForm.reset();
 };
 
-document.querySelector('.palindrome-checker').addEventListener('submit', e => {
+$palindromeForm.addEventListener('submit', e => {
   e.preventDefault();
-
-  render($palindromeInput.value);
+  renderResult($palindromeInput.value);
 });
+
+// [변경사항]
+// - $palindromeChecker 변수명을 $palindromeForm으로 변경해 의미를 명확히 함
+// - isPalindrome 함수 내에서 가독성을 위해 한 줄로 작성한 코드를 여러 줄로 나누어서 작성하고 변수를 추가함
+// - eventListener 내부에서 $palindromeResult의 innerHTML을 갱신하는 부분을 renderResult 함수로 분리
+// - $palindromeInput의 value를 빈 문자열로 초기화해주는 코드를 form의 reset 메서드로 변경하여 의미를 명확히 함
